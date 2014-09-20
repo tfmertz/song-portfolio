626497ae4d8cab51d080c0db2202dc0d6af2db47
Tom Mertz <tom.mertz@verizon.net>
2014-09-20 12:59:28 -0400

    Add styles to overlay

--- js/overlay.js
+++ js/overlay.js
@@ -2,15 +2,29 @@
 
 //make the overlay div
 var $overlay = $('<div id="overlay"></div>');
+var $close = $('<a href="#" id="close-overlay">Close</a>');
+
+var $about = $('<div id="about"></div');
+
+//append button to overlay
+$overlay.append($close);
+$overlay.append($about);
 
 //append it to the body, css hides it
 $("body").append($overlay);
 
 //when the about or contact link is clicked
-$(".about").click(function(){
+$(".overlay").click(function(event){
 	//prevent the page from going to the about.html
 	event.preventDefault();
 	//show the overlay
 	$overlay.show();
+
+});
+
+//if the close button is pushed
+$("#close-overlay").click(function(){
+	//hide the overlay
+	$overlay.hide();
 
 });
